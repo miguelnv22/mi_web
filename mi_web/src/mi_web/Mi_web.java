27d08b1c63b26d332f1a2c8bbe7b621a5834d005
miguelnv22 <miguelnv22@gmail.com>
2019-01-17 13:01:13 +0100

    Creado archivo java

---
+++ mi_web/src/mi_web/Mi_web.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mi_web;
+
+/**
+ *
+ * @author root-admin
+ */
+public class Mi_web {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
